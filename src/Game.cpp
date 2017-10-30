621600b3a3d5e4dd817dc165ff245305d6899780
andreataglia <drew.taglia@gmail.com>
2017-10-30 18:33:21 +0100

    Game class started

---
+++ src/Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Spranga on 30-Oct-17.
+//
+
+#include "Game.h"
---
+++ src/Game.h
@@ -0,0 +1,17 @@
+//
+// Created by Spranga on 30-Oct-17.
+//
+
+#ifndef SCOPA_GAME_H
+#define SCOPA_GAME_H
+
+
+class Game {
+public:
+    
+
+    State currentState;
+};
+
+
+#endif //SCOPA_GAME_H
