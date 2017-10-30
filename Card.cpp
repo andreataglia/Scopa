25e22e79f1b10b44c27ac4942be612a8c6a7cd65
Uino95 <teo.sinik@yahoo.it>
2017-10-30 16:48:46 +0100

    First class created

---
+++ Card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo Sinico on 30/10/17.
+//
+
+#include "Card.h"
---
+++ Card.h
@@ -0,0 +1,14 @@
+//
+// Created by Matteo Sinico on 30/10/17.
+//
+
+#ifndef SCOPA_CARD_H
+#define SCOPA_CARD_H
+
+
+class Card {
+
+};
+
+
+#endif //SCOPA_CARD_H
